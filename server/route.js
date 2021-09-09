3da8193d93871be72d534ace8f87ddb0ee607a2b
Alex Roberson <ajroberson321@gmail.com>
2021-09-09 16:08:28 -0700

    route.js now formatted using eslint

--- server/route.js
+++ server/route.js
@@ -1,106 +1,106 @@
-var router = require('express').Router();
-var axios = require('axios');
-var {TOKEN} = require('../config.js');
+const router = require('express').Router();
+const axios = require('axios');
+const { TOKEN } = require('../config');
 
 router.route('/reviews/:product_id')
-  .get(function (req, res) {
-    var data = '';
+  .get((req, res) => {
+    const data = '';
 
-    var config = {
+    const config = {
       method: 'get',
       url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/reviews?product_id=${req.params.product_id}`,
       headers: {
-        'Authorization': TOKEN
+        Authorization: TOKEN,
       },
-      data : data
+      data,
     };
 
     axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-      res.status(200).send(JSON.stringify(response.data));
-      res.end();
-    })
-    .catch(function (error) {
-      res.status(400).send()
-      console.log(error);
-    });
-  })
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        res.status(200).send(JSON.stringify(response.data));
+        res.end();
+      })
+      .catch((error) => {
+        res.status(400).send();
+        console.log(error);
+      });
+  });
 //   .post(function)
 
 router.route('/products/:product_id')
-   .get(function (req, res) {
-    var data = '';
+  .get((req, res) => {
+    const data = '';
 
-    var config = {
+    const config = {
       method: 'get',
       url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products?product_id=${req.params.product_id}`,
       headers: {
-        'Authorization': TOKEN
+        Authorization: TOKEN,
       },
-      data : data
+      data,
     };
 
     axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-      res.status(200).send(JSON.stringify(response.data));
-      res.end();
-    })
-    .catch(function (error) {
-      res.status(400).send();
-      console.log(error);
-    });
-   })
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        res.status(200).send(JSON.stringify(response.data));
+        res.end();
+      })
+      .catch((error) => {
+        res.status(400).send();
+        console.log(error);
+      });
+  });
 
 router.route('/related/:product_id')
-   .get(function (req, res) {
-    var data = '';
+  .get((req, res) => {
+    const data = '';
 
-    var config = {
+    const config = {
       method: 'get',
       url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products?product_id=${req.params.product_id}/related`,
       headers: {
-        'Authorization': TOKEN
+        Authorization: TOKEN,
       },
-      data : data
+      data,
     };
 
     axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-      res.status(200).send(JSON.stringify(response.data));
-      res.end();
-    })
-    .catch(function (error) {
-      res.status(400).send();
-      console.log(error);
-    });
-   })
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        res.status(200).send(JSON.stringify(response.data));
+        res.end();
+      })
+      .catch((error) => {
+        res.status(400).send();
+        console.log(error);
+      });
+  });
 
 router.route('/questions/:product_id')
-   .get(function (req, res) {
-    var data = '';
+  .get((req, res) => {
+    const data = '';
 
-    var config = {
+    const config = {
       method: 'get',
       url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products?product_id=${req.params.product_id}/qa/questions`,
       headers: {
-        'Authorization': TOKEN
+        Authorization: TOKEN,
       },
-      data : data
+      data,
     };
 
     axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-      res.status(200).send(JSON.stringify(response.data));
-      res.end();
-    })
-    .catch(function (error) {
-      res.status(400).send();
-      console.log(error);
-    });
-   })
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        res.status(200).send(JSON.stringify(response.data));
+        res.end();
+      })
+      .catch((error) => {
+        res.status(400).send();
+        console.log(error);
+      });
+  });
 
 module.exports = router;
