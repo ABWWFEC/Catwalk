9fe42f4fc81ccfdce129af0c7525263adbee95bd
Alex Roberson <ajroberson321@gmail.com>
2021-09-09 12:01:17 -0700

    initial server routes partially working

--- server/route.js
+++ server/route.js
@@ -1,11 +1,63 @@
 var router = require('express').Router();
+var axios = require('axios');
+var {TOKEN} = require('../config.js');
 
-router.route('')
-  .get(function)
-  .post(function)
+router.route('/reviews/:product_id')
+  .get(function (req, res) {
+    var data = '';
 
-router.route('')
-  .get(function)
-  .post(function)
+    var config = {
+      method: 'get',
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/reviews?product_id=${req.params.product_id}`,
+      headers: {
+        'Authorization': TOKEN
+      },
+      data : data
+    };
+
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      res.status(200).send(JSON.stringify(response.data));
+      res.end();
+    })
+    .catch(function (error) {
+      res.status(400).send()
+      console.log(error);
+    });
+  })
+//   .post(function)
+
+router.route('/products/:product_id')
+   .get(function (req, res) {
+    var data = '';
+
+    var config = {
+      method: 'get',
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products?product_id=${req.params.product_id}`,
+      headers: {
+        'Authorization': TOKEN
+      },
+      data : data
+    };
+
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      res.status(200).send(JSON.stringify(response.data));
+      res.end();
+    })
+    .catch(function (error) {
+      res.status(400).send();
+      console.log(error);
+    });
+   })
+
+// router.route('/related')
+//   .get(function)
+
+// router.route('/questions')
+//   .get(function)
+//   .post(function)
 
 module.exports = router;
