bc0c23cb7bb601d1d3da12440a92c93747887529
WalterT-MK <tangtgs1516993@gmail.com>
2021-09-10 11:53:00 -0700

    changes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const morgan = require('morgan');
 const route = require('./route.js');
 const path = require('path');
-const morgan = require('morgan');
 const app = express();
 
 const port = 3000;
