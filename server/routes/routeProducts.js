4de5a9ffa7d15fa19565d8e1b0c4c3992ae44953
WalterT-MK <tangtgs1516993@gmail.com>
2021-09-10 15:02:02 -0700

    updated routeProducts

--- server/routes/routeProducts.js
+++ server/routes/routeProducts.js
@@ -1,6 +1,31 @@
+/* eslint-disable no-console */
 const router = require('express').Router();
 const axios = require('axios');
 const { TOKEN } = require('../../config');
+
+router.route('/products')
+  .get((req, res) => {
+    const data = '';
+
+    const config = {
+      method: 'get',
+      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products',
+      headers: {
+        Authorization: TOKEN,
+      },
+      data,
+    };
+
+    axios(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        res.status(200).send(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        res.status(400).send();
+        console.log(error);
+      });
+  });
 
 router.route('/products/:product_id')
   .get((req, res) => {
@@ -8,7 +33,7 @@ router.route('/products/:product_id')
 
     const config = {
       method: 'get',
-      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products?product_id=${req.params.product_id}`,
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${req.params.product_id}`,
       headers: {
         Authorization: TOKEN,
       },
