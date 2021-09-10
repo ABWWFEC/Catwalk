6952279210223ae4d29da101adcc659e3b401561
Alex Roberson <ajroberson321@gmail.com>
2021-09-10 11:54:11 -0700

    separate files filled need to be formatted

--- server/routes/routeProducts.js
+++ server/routes/routeProducts.js
@@ -1,3 +1,30 @@
 const router = require('express').Router();
 const axios = require('axios');
 const { TOKEN } = require('../../config');
+
+router.route('/products/:product_id')
+  .get((req, res) => {
+    const data = '';
+
+    const config = {
+      method: 'get',
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products?product_id=${req.params.product_id}`,
+      headers: {
+        Authorization: TOKEN,
+      },
+      data,
+    };
+
+    axios(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        res.status(200).send(JSON.stringify(response.data));
+        res.end();
+      })
+      .catch((error) => {
+        res.status(400).send();
+        console.log(error);
+      });
+  });
+
+module.exports = router;
--- server/routes/routeReviews.js
+++ server/routes/routeReviews.js
@@ -62,31 +62,6 @@ router.route('/reviews/:product_id')
       });
   });
 
-router.route('/products/:product_id')
-  .get((req, res) => {
-    const data = '';
-
-    const config = {
-      method: 'get',
-      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products?product_id=${req.params.product_id}`,
-      headers: {
-        Authorization: TOKEN,
-      },
-      data,
-    };
-
-    axios(config)
-      .then((response) => {
-        console.log(JSON.stringify(response.data));
-        res.status(200).send(JSON.stringify(response.data));
-        res.end();
-      })
-      .catch((error) => {
-        res.status(400).send();
-        console.log(error);
-      });
-  });
-
 router.route('/related/:product_id')
   .get((req, res) => {
     const data = '';
@@ -109,61 +84,6 @@ router.route('/related/:product_id')
       .catch((error) => {
         res.status(400).send();
         console.log(error);
-      });
-  });
-
-router.route('/questions/:product_id')
-  .get((req, res) => {
-    const data = '';
-
-    const config = {
-      method: 'get',
-      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products?product_id=${req.params.product_id}/qa/questions`,
-      headers: {
-        Authorization: TOKEN,
-      },
-      data,
-    };
-
-    axios(config)
-      .then((response) => {
-        console.log(JSON.stringify(response.data));
-        res.status(200).send(JSON.stringify(response.data));
-        res.end();
-      })
-      .catch((error) => {
-        res.status(400).send();
-        console.log(error);
-      });
-  })
-  .post((req, res) => {
-    const {
-      body, name, email, product_id,
-    } = req.body;
-    const data = {
-      body,
-      name,
-      email,
-      product_id,
-    };
-
-    const config = {
-      method: 'post',
-      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products?product_id=${req.params.product_id}/qa/questions`,
-      headers: {
-        Authorization: TOKEN,
-      },
-      data,
-    };
-
-    axios(config)
-      .then((response) => {
-        console.log(JSON.stringify(response.data));
-        res.status(201).send('CREATED');
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(400).send(error);
       });
   });
 
