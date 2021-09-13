a0ef5d4c0ca19e01cb7dabb539883f8ff0930ce8
Alex Roberson <ajroberson321@gmail.com>
2021-09-13 16:22:32 -0700

    add barebones structure to relatedList and relatedEntry system, without carousel

--- client/src/components/Related_Prods.jsx
+++ client/src/components/Related_Prods.jsx
@@ -1,3 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
-export default;
+const relatedList = ({relatedProds}) => {
+  const getRelated = (prodId) => {
+    var data = '';
+
+    var config = {
+      method: 'get',
+      url: `127.0.0.1:3000/api/product/${prodId}/related`,
+      headers: {},
+      data : data
+    };
+
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+
+  const [related, setRelated] = useState([]);
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <div className="relatedList">
+      {relatedProds.map(product =>
+        <relatedEntry product={product} key={product.id} />
+      )}
+   </div>
+  )
+}
+export default related = relatedList;
---
+++ client/src/components/relatedEntry.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+var relatedEntry = ({product}) => (
+
+)
+
+export default relatedEntry;
