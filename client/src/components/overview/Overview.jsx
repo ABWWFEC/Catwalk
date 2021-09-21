d1ebbe5fb26dcd8bd181510a5b78450ead188196
WalterT-MK <tangtgs1516993@gmail.com>
2021-09-20 21:38:40 -0700

    Image gallery complete, implemented zoom feature

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -2,11 +2,12 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Cart from './Cart.jsx';
 import Style from './Style.jsx';
+import Gallery from './Gallery test.jsx';
 
 const Overview = ({prodId}) => {
   const [prodInfo, setProdInfo] = useState({});
   const [prodStyle, setProdStyle] = useState([]);
-  const [selStyle, setSelStyle] = useState({skus:{}});
+  const [selStyle, setSelStyle] = useState({skus:{}, photos: [{url: null, thumbnail_url: null}]});
 
   useEffect(() => {
     const getInfo = () => {
@@ -36,26 +37,33 @@ const Overview = ({prodId}) => {
   }, [prodId]);
 
   return (
-    <div className='Overview'>
-      <div className='images'>
-      {console.log(prodStyle)}
-      </div>
-      <div className='info'>
-        <div className='rating'>
-          <div className='rate star'></div>
-          <div className='all review'></div>
-        </div>
-        <div className='category'>{prodInfo.category}</div>
-        <h1 className='title'>{prodInfo.name}</h1>
-        <h1 className='price'>{`$ ${selStyle.original_price}`}</h1>
-        <h1 className='selected style'>{`STYLE > ${selStyle.name}`}</h1>
-        <div className='style seletcer'>
-          <Style styles={prodStyle} setStyle={setSelStyle}/>
-        </div>
-        <div className='cart'>
-          <Cart style={selStyle} prodId={prodId}/>
+    <div className='container' style={{width: '100%'}}>
+      <div className='row'>
+        <div className='col-8'>
+          <div className='images'>
+            <Gallery style={selStyle}/>
+          </div>
         </div>
 
+        <div className='col-4'>
+          <div className='info'>
+            <div className='rating'>
+              <div className='rate star'></div>
+              <div className='all review'></div>
+            </div>
+            <div className='category'>{prodInfo.category}</div>
+            <h1 className='title'>{prodInfo.name}</h1>
+            <h1 className='price'>{`$ ${selStyle.original_price}`}</h1>
+            <h1 className='selected style'>{`STYLE > ${selStyle.name}`}</h1>
+            <div className='style seletcer'>
+              <Style styles={prodStyle} setStyle={setSelStyle}/>
+            </div>
+            <div className='cart'>
+              <Cart style={selStyle} prodId={prodId}/>
+            </div>
+
+          </div>
+        </div>
       </div>
     </div>
   )
