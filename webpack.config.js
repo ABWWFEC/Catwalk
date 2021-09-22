cdab404ec78097155a1cb333da2931098f5ec545
WalterT-MK <tangtgs1516993@gmail.com>
2021-09-21 19:29:46 -0700

    webpack set up

--- webpack.config.js
+++ webpack.config.js
@@ -18,14 +18,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: path.resolve(__dirname, 'node_modules')
         options: {
-<<<<<<< HEAD
           presets: ['@babel/preset-env', '@babel/preset-react'],
-=======
-          presets: ['@babel/preset-react', '@babel/preset-env'],
-        },
-        resolve: {
-          extensions: ['.jsx', '.js'],
->>>>>>> 55d5893603ad3770eef3ff64252153a99bbcec9a
         },
       },
       {
